63ae8ee2e40b25a7222138bb7bac00d1af883cc1
Fatima <fatimavaliyeva0704@gmail.com>
2024-05-14 00:42:00 +0400

    Fact about Jupiter added.

--- homework/Program.cs
+++ homework/Program.cs
@@ -5,6 +5,7 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, Universe!");
+            Console.WriteLine("Did you know that Jupiter has 95 moons?");
         }
     }
 }
