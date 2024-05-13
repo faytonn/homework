69dcc06bb6fb0d73ba307ab4e3929f8eb9f21648
Fatima <fatimavaliyeva0704@gmail.com>
2024-05-14 00:47:03 +0400

    Number of jupiter and saturn in integer data type given.

--- homework/Program.cs
+++ homework/Program.cs
@@ -6,6 +6,9 @@
         {
             Console.WriteLine("Hello, Universe!");
             Console.WriteLine("Did you know that Jupiter has 95 moons?");
+            int Jupiter_moons = 95;
+            int Saturn_moons = 146;
+            int Total_moons = Jupiter_moons + Saturn_moons; 
         }
     }
 }
