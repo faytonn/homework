f9dd2af600bee6d3e46415ffbf7e2d3d88f51f64
Fatima <fatimavaliyeva0704@gmail.com>
2024-05-14 09:37:35 +0400

    Largest moons in these planets added.

--- homework/Program.cs
+++ homework/Program.cs
@@ -8,7 +8,10 @@
             Console.WriteLine("Did you know that Jupiter has 95 moons?");
             int Jupiter_moons = 95;
             int Saturn_moons = 146;
-            int Total_moons = Jupiter_moons + Saturn_moons; 
+            int Total_moons = Jupiter_moons + Saturn_moons;
+            Console.WriteLine("The total number of moons in Jupiter and Saturn are" + Total_moons);
+            string largest_saturn_moon = "Titan";
+            string largest_jupiter_moon = "Ganymede";
         }
     }
 }
